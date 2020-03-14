89c3fd8e0b910544de50000bde3674eddf64fb25
Hygor X. Araújo <dev.hygor@gmail.com>
2020-03-14 20:52:53 -0300

    Create contacts for speakers

---
+++ eventex/core/tests/test_model_contact.py
@@ -0,0 +1,38 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from eventex.core.models import Speaker, Contact
+
+
+class ContactModelTest(TestCase):
+    def setUp(self) -> None:
+        self.speaker = Speaker.objects.create(
+            name='John Doe',
+            slug='john-doe',
+            photo='',
+        )
+
+    def test_email(self):
+        contact = Contact.objects.create(
+            speaker=self.speaker,
+            kind=Contact.EMAIL,
+            value='john@example.com',
+        )
+        self.assertTrue(Contact.objects.exists())
+
+    def test_phone(self):
+        contact = Contact.objects.create(
+            speaker=self.speaker,
+            kind=Contact.PHONE,
+            value='32-999999999',
+        )
+        self.assertTrue(Contact.objects.exists())
+
+    def test_choices(self):
+        """Contact kind should be limited to E or P"""
+        contact = Contact(speaker=self.speaker, kind='A', value='B')
+        self.assertRaises(ValidationError, contact.full_clean)
+
+    def test_str(self):
+        contact = Contact(speaker=self.speaker, kind=Contact.EMAIL, value='john@example.com')
+        self.assertEqual('john@example.com', str(contact))
